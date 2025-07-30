7fb542bee3bcdce37d23b1b76a1f9252e07edf1f
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-07-29 23:11:52 -0400

    Editor template

--- src/components/symbol_library/SymbolCreator.tsx
+++ src/components/symbol_library/SymbolCreator.tsx
@@ -10,11 +10,21 @@ interface props_SymbolCreator {
 
 export default function SymbolCreator({show, Add, Hide}: props_SymbolCreator)
 {
-    const [newSymbol, setNewSymbol] = useState<SymbolData>();
+    const [newSymbol, setNewSymbol] = useState<SymbolData>({
+        colour: "white",
+        shape: "circle",
+        text: ""
+    });
 
     return (
         <div className={show ? "component-symbolcreator": "hidden"}>
-            <input type="button" id="symbolcreatoreclose" value="X" onClick={() => Hide()}/>
+            <input type="button" id="symbolcreatorclose" value="X" onClick={() => Hide()}/>
+            <div className="display">
+                <Symbol symbol={newSymbol}/>
+                <form className="editor">
+
+                </form>
+            </div>
             <input type="button" id="addnewsymbol" value="Save Symbol" onClick={() => Add(newSymbol)}/>
         </div>
     );
