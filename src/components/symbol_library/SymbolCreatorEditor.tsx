ca767b3ba1d230ab3471dd9f737d2c85cb8424c3
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-07-30 19:46:01 -0400

    Controls for shape and text content

--- src/components/symbol_library/SymbolCreatorEditor.tsx
+++ src/components/symbol_library/SymbolCreatorEditor.tsx
@@ -10,8 +10,15 @@ export default function SymbolCreatorEditor({symbol, SetSymbol}: props_SymbolCre
     return (
         <div id="component-symbolcreatoreditor">
             <form>
-                <input type="color" value={symbol.colour} onChange={(e) => SetSymbol({...symbol, colour: e.target.value})}/>
-                
+                <label htmlFor="editor-colour">Colour:</label>
+                <input type="color" id="editor-colour" value={symbol.colour} onChange={(e) => SetSymbol({...symbol, colour: e.target.value})}/>
+                <label htmlFor="editor-shape">Shape:</label>
+                <select id="editor-shape" value={symbol.shape} onChange={(e) => SetSymbol({...symbol, shape: e.target.value})}>
+                    <option value="circle">Circle</option>
+                    <option value="square">Square</option>
+                </select>
+                <label htmlFor="editor-text">Icon:</label>
+                <input type="text" id="editor-text" value={symbol.text} onChange={(e) => SetSymbol({...symbol, text: e.target.value})}/>                
             </form>
         </div>
     )
