1900ae4f29d866e525e9638abc89db85e0693685
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-07-29 23:05:41 -0400

    Import symbol creator for testing

--- src/components/page/DesignPane.tsx
+++ src/components/page/DesignPane.tsx
@@ -1,8 +1,10 @@
+import SymbolLibraryPane from "../symbol_library/SymbolLibraryPane";
+
 export default function DesignPane()
 {
     return(
         <div className="component-designpane">
-            
+            <SymbolLibraryPane/>
         </div>
     );
 }
