d174e8d5563431ff2096ad835896270d8e3d2fa4
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-07-30 20:33:37 -0400

    Some styling for creator menu

--- src/components/symbol_library/SymbolCreatorEditor.tsx
+++ src/components/symbol_library/SymbolCreatorEditor.tsx
@@ -1,5 +1,7 @@
 import { type SymbolData } from "./Symbol"
 
+import './styles/symbol_creator_editor.css'
+
 interface props_SymbolCreatorEditor {
     symbol: SymbolData,
     SetSymbol: Function
@@ -10,17 +12,25 @@ export default function SymbolCreatorEditor({symbol, SetSymbol}: props_SymbolCre
     return (
         <div id="component-symbolcreatoreditor">
             <form>
-                <label htmlFor="editor-shape">Shape:</label>
-                <select id="editor-shape" value={symbol.shape} onChange={(e) => SetSymbol({...symbol, shape: e.target.value})}>
-                    <option value="circle">Circle</option>
-                    <option value="square">Square</option>
-                </select>
-                <label htmlFor="editor-colour">Colour:</label>
-                <input type="color" id="editor-colour" value={symbol.colour} onChange={(e) => SetSymbol({...symbol, colour: e.target.value})}/>
-                <label htmlFor="editor-text">Icon:</label>
-                <input type="text" id="editor-text" value={symbol.text} onChange={(e) => SetSymbol({...symbol, text: e.target.value})}/>
-                <label htmlFor="editor-text-colour">Colour:</label>
-                <input type="color" id="editor-text-colour" value={symbol.textColour} onChange={(e) => SetSymbol({...symbol, textColour: e.target.value})}/>           
+                <div>
+                    <label htmlFor="editor-shape">Shape:</label>
+                    <select id="editor-shape" value={symbol.shape} onChange={(e) => SetSymbol({...symbol, shape: e.target.value})}>
+                        <option value="circle">Circle</option>
+                        <option value="square">Square</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="editor-colour">Colour:</label>
+                    <input type="color" id="editor-colour" value={symbol.colour} onChange={(e) => SetSymbol({...symbol, colour: e.target.value})}/>
+                </div>
+                <div>
+                    <label htmlFor="editor-text">Icon:</label>
+                    <input type="text" id="editor-text" value={symbol.text} onChange={(e) => SetSymbol({...symbol, text: e.target.value})}/>
+                </div>
+                <div>    
+                    <label htmlFor="editor-text-colour">Colour:</label>
+                    <input type="color" id="editor-text-colour" value={symbol.textColour} onChange={(e) => SetSymbol({...symbol, textColour: e.target.value})}/>
+                </div>       
             </form>
         </div>
     )
