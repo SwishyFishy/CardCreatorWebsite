1edb9b125458bea80d818cbe59462b72ccb9ae83
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 15:01:26 -0400

    Use new icon export

--- src/components/symbol_library/BasicSymbols.ts
+++ src/components/symbol_library/BasicSymbols.ts
@@ -1,11 +1,4 @@
-import icon_up from '../../assets/icon_arrowup.png';
-import icon_down from '../../assets/icon_arrowdown.png';
-import icon_left from '../../assets/icon_arrowleft.png';
-import icon_right from '../../assets/icon_arrowright.png';
-import icon_badge from '../../assets/icon_badge.png';
-import icon_nut from '../../assets/icon_nut.png';
-import icon_sun from '../../assets/icon_sun.png';
-import icon_tree from '../../assets/icon_tree.png';
+import { images } from '../../assets/icons/icons.ts';
 
 import { type SymbolData } from "./Symbol";
 
@@ -13,42 +6,42 @@ const BasicSymbols: SymbolData[] = [
     {
         shape: "square",
         colour: "grey",
-        icon: icon_up
+        icon: images.icon_up
     },
     {
         shape: "square",
         colour: "grey",
-        icon: icon_down
+        icon: images.icon_down
     },
     {
         shape: "square",
         colour: "grey",
-        icon: icon_left
+        icon: images.icon_left
     },
     {
         shape: "square",
         colour: "grey",
-        icon: icon_right
+        icon: images.icon_right
     },
     {
         shape: "circle",
         colour: "beige",
-        icon: icon_badge
+        icon: images.icon_badge
     },
     {
         shape: "circle",
         colour: "silver",
-        icon: icon_nut
+        icon: images.icon_nut
     },
     {
         shape: "circle",
         colour: "yellow",
-        icon: icon_sun
+        icon: images.icon_sun
     },
     {
         shape: "circle",
         colour: "green",
-        icon: icon_tree
+        icon: images.icon_tree
     }
 ];
 
