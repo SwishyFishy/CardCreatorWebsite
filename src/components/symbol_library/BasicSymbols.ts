190a5cd4bebf7a9c246f4366f9beabb9fe6dc9dc
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 14:34:34 -0400

    Remove poor icons

--- src/components/symbol_library/BasicSymbols.ts
+++ src/components/symbol_library/BasicSymbols.ts
@@ -3,14 +3,9 @@ import icon_down from '../../assets/icon_arrowdown.png';
 import icon_left from '../../assets/icon_arrowleft.png';
 import icon_right from '../../assets/icon_arrowright.png';
 import icon_badge from '../../assets/icon_badge.png';
-import icon_banner from '../../assets/icon_banner.png';
-import icon_chalice from '../../assets/icon_chalice.png';
 import icon_nut from '../../assets/icon_nut.png';
-import icon_shield from '../../assets/icon_shield.png';
-import icon_star from '../../assets/icon_star.png';
 import icon_sun from '../../assets/icon_sun.png';
 import icon_tree from '../../assets/icon_tree.png';
-import icon_wrench from '../../assets/icon_wrench.png';
 
 import { type SymbolData } from "./Symbol";
 
@@ -42,28 +37,8 @@ const BasicSymbols: SymbolData[] = [
     },
     {
         shape: "circle",
-        colour: "purple",
-        icon: icon_banner
-    },
-    {
-        shape: "circle",
-        colour: "pink",
-        icon: icon_chalice
-    },
-    {
-        shape: "circle",
         colour: "silver",
         icon: icon_nut
-    },
-    {
-        shape: "circle",
-        colour: "brown",
-        icon: icon_shield
-    },
-    {
-        shape: "circle",
-        colour: "red",
-        icon: icon_star
     },
     {
         shape: "circle",
@@ -74,11 +49,6 @@ const BasicSymbols: SymbolData[] = [
         shape: "circle",
         colour: "green",
         icon: icon_tree
-    },
-    {
-        shape: "circle",
-        colour: "blue",
-        icon: icon_wrench
     }
 ];
 
