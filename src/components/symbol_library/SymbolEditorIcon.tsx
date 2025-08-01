a651aa3db4b863335293486722b26a2f57a5fedd
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 17:24:14 -0400

    Symbol uploading styling, and change removal functionality

--- src/components/symbol_library/SymbolEditorIcon.tsx
+++ src/components/symbol_library/SymbolEditorIcon.tsx
@@ -19,19 +19,20 @@ export default function SymbolEditorIcon({symbol, SetSymbol}: props_SymbolEditor
     return (
         <div id="component-symboleditoricon">
             <form>
-                <h1>Icons</h1>
-                <div>
+                <h1>Icons:</h1>
+                <div className="collection">
                     {symbolLibrary.map((image, index) => (
-                        <input type="image" key={index} src={image} onClick={(e) => {e.preventDefault(); SetSymbol({...symbol, icon: image})}}/>
+                        <input type="image" key={index} src={image} onClick={(e) => { e.preventDefault(); SetSymbol({...symbol, icon: symbol.icon == image ? "" : image}); }}/>
                     ))}
                 </div>
-
                 <Control>
-                    <label htmlFor="editor-icon">Import:</label>
-                    <input type="file" id="editor-icon" accept=".png" onChange={(e) => setSymbolLibrary([...symbolLibrary, URL.createObjectURL(e.target.files![0])])}/>
+                    <label htmlFor="editor-icon">Import Icons:</label>
+                    <input type="file" id="editor-icon" accept=".png" onChange={(e) => {
+                        const icon: string = URL.createObjectURL(e.target.files![0]);
+                        setSymbolLibrary([...symbolLibrary, icon]);
+                        SetSymbol({...symbol, icon: icon});
+                    }}/>
                 </Control>
-                
-                <input type="button" id="editor-icon-reset" value="Remove Icon" onClick={() => SetSymbol({...symbol, icon: ""})}/>
             </form>
         </div>
     );
