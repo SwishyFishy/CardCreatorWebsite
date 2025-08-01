b603e48e7aa20a6fb439b5b53d5292faf9dcf9d4
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 16:05:25 -0400

    Layout and functionality of icon library and editor

--- src/components/symbol_library/SymbolEditorIcon.tsx
+++ src/components/symbol_library/SymbolEditorIcon.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+import symbols from "../../assets/icons/icons.ts";
 import { type SymbolData } from "./Symbol";
 
 import './styles/symbol_editor_icon.css';
@@ -9,16 +12,24 @@ interface props_SymbolEditorIcon {
 
 export default function SymbolEditorIcon({symbol, SetSymbol}: props_SymbolEditorIcon)
 {
+    const [symbolLibrary, setSymbolLibrary] = useState<string[]>(Object.values(symbols));
+
     return (
         <div id="component-symboleditoricon">
             <form>
-                <div className="control">
-                    <label htmlFor="editor-icon">Icon:</label>
-                    <div className="resettableinput">
-                        <input type="file" id="editor-icon" accept=".png" onChange={(e) => SetSymbol({...symbol, icon: URL.createObjectURL(e.target.files![0])})}/>
-                        <input type="button" id="editor-icon-reset" value="Remove" onClick={() => SetSymbol({...symbol, icon: ""})}/>
-                    </div>
+                <h1>Icons</h1>
+                <div>
+                    {symbolLibrary.map((image, index) => (
+                        <input type="image" key={index} src={image} onClick={(e) => {e.preventDefault(); SetSymbol({...symbol, icon: image})}}/>
+                    ))}
                 </div>
+
+                <div className="control">
+                    <label htmlFor="editor-icon">Import:</label>
+                    <input type="file" id="editor-icon" accept=".png" onChange={(e) => setSymbolLibrary([...symbolLibrary, URL.createObjectURL(e.target.files![0])])}/>
+                </div>
+                
+                <input type="button" id="editor-icon-reset" value="Remove Icon" onClick={() => SetSymbol({...symbol, icon: ""})}/>
             </form>
         </div>
     );
