f3ce27ac60cde829d0475473153f4e0fcffc8318
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 15:00:53 -0400

    Move icons into own folder in assets, and export all from a single icon.ts file therein

---
+++ src/assets/icons/icons.ts
@@ -0,0 +1,11 @@
+import icon_up from './icon_arrowup.png';
+import icon_down from './icon_arrowdown.png';
+import icon_left from './icon_arrowleft.png';
+import icon_right from './icon_arrowright.png';
+import icon_badge from './icon_badge.png';
+import icon_nut from './icon_nut.png';
+import icon_sun from './icon_sun.png';
+import icon_tree from './icon_tree.png';
+
+export const images = {icon_up, icon_down, icon_left, icon_right, icon_badge, icon_nut, icon_sun, icon_tree}
+export const imageArray = [icon_up, icon_down, icon_left, icon_right, icon_badge, icon_nut, icon_sun, icon_tree];
