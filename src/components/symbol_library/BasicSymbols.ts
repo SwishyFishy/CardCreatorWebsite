e03bdc6bde9f5f5898ff28d2b893a2b524eb80d4
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-01 14:24:16 -0400

    Basic symbol library

--- src/components/symbol_library/BasicSymbols.ts
+++ src/components/symbol_library/BasicSymbols.ts
@@ -1,22 +1,84 @@
-import image from '../../assets/planeswalkersymboltransparent.png';
+import icon_up from '../../assets/icon_arrowup.png';
+import icon_down from '../../assets/icon_arrowdown.png';
+import icon_left from '../../assets/icon_arrowleft.png';
+import icon_right from '../../assets/icon_arrowright.png';
+import icon_badge from '../../assets/icon_badge.png';
+import icon_banner from '../../assets/icon_banner.png';
+import icon_chalice from '../../assets/icon_chalice.png';
+import icon_nut from '../../assets/icon_nut.png';
+import icon_shield from '../../assets/icon_shield.png';
+import icon_star from '../../assets/icon_star.png';
+import icon_sun from '../../assets/icon_sun.png';
+import icon_tree from '../../assets/icon_tree.png';
+import icon_wrench from '../../assets/icon_wrench.png';
 
 import { type SymbolData } from "./Symbol";
 
 const BasicSymbols: SymbolData[] = [
     {
-        shape: "circle",
-        colour: "blue",
-        text: "A"
+        shape: "square",
+        colour: "grey",
+        icon: icon_up
     },
     {
         shape: "square",
-        colour: "green",
-        icon: image
+        colour: "grey",
+        icon: icon_down
+    },
+    {
+        shape: "square",
+        colour: "grey",
+        icon: icon_left
+    },
+    {
+        shape: "square",
+        colour: "grey",
+        icon: icon_right
+    },
+    {
+        shape: "circle",
+        colour: "beige",
+        icon: icon_badge
+    },
+    {
+        shape: "circle",
+        colour: "purple",
+        icon: icon_banner
+    },
+    {
+        shape: "circle",
+        colour: "pink",
+        icon: icon_chalice
+    },
+    {
+        shape: "circle",
+        colour: "silver",
+        icon: icon_nut
+    },
+    {
+        shape: "circle",
+        colour: "brown",
+        icon: icon_shield
+    },
+    {
+        shape: "circle",
+        colour: "red",
+        icon: icon_star
     },
     {
         shape: "circle",
         colour: "yellow",
-        text: "ر.ع."
+        icon: icon_sun
+    },
+    {
+        shape: "circle",
+        colour: "green",
+        icon: icon_tree
+    },
+    {
+        shape: "circle",
+        colour: "blue",
+        icon: icon_wrench
     }
 ];
 
