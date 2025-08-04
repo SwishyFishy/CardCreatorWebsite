4602eae09f944084f0a3efd913b7efc92d40b59c
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-03 21:12:12 -0400

    Basic header links

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -2,7 +2,11 @@ export default function Header()
 {
     return(
         <div id="component-header">
-
+            <h1>CardCreator</h1>
+            <div>
+                <a href="https://github.com/SwishyFishy/CardCreatorWebsite.git" target="__blank">Contribute</a>
+                <a href="mailto:jonahgallowayfenwick@protonmail.com">Contact the Creator</a>
+            </div>
         </div>
     );
 }
