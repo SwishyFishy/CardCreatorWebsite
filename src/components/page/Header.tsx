613c10dc1c763a7fc32b2282196fb60402010054
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-05 10:41:08 -0400

    Header styles

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -1,9 +1,11 @@
+import './styles/header.css';
+
 export default function Header()
 {
     return(
         <div id="component-header">
             <h1>CardCreator</h1>
-            <div>
+            <div className="nav">
                 <a href="https://github.com/SwishyFishy/CardCreatorWebsite.git" target="__blank">Contribute</a>
                 <a href="mailto:jonahgallowayfenwick@protonmail.com">Contact the Creator</a>
             </div>
