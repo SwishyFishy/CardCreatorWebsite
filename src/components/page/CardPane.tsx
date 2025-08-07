b95ef1bc4e321510df0ff95b28626841924146ce
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-07 15:23:37 -0400

    CardPane setup & test

--- src/components/page/CardPane.tsx
+++ src/components/page/CardPane.tsx
@@ -1,8 +1,12 @@
+import Card from "../card/Card";
+
+import './styles/cardpane.css';
+
 export default function CardPane()
 {
     return(
         <div id="component-cardpane">
-            
+            <Card/>
         </div>
     );
 }
