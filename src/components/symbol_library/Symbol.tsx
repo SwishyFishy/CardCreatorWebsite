5fada5f60f4f563e6039d87fbc3eb35aa302eb32
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-20 23:17:19 -0400

    Change gradient to be consistent with other implementations

--- src/components/symbol_library/Symbol.tsx
+++ src/components/symbol_library/Symbol.tsx
@@ -16,11 +16,11 @@ interface props_Symbol {
 
 export default function Symbol({symbol}: props_Symbol)
 {
-    const symbolColour: string = `linear-gradient(135deg, ${symbol.colour}, ${symbol.gradient})`;
+    const symbolColour: string = `linear-gradient(135deg, ${symbol.colour}, ${symbol.useGradient ? symbol.gradient : symbol.colour})`;
     const iconColour: string = symbol.textColour || "black";
 
     return(
-        <div id="component-symbol" className={`symbol-${symbol.shape}`} style={symbol.useGradient ? {backgroundImage: symbolColour} : {backgroundColor: symbol.colour}}>
+        <div id="component-symbol" className={`symbol-${symbol.shape}`} style={{backgroundImage: symbolColour}}>
             {<span style={{color: iconColour, fontSize: symbol.text && symbol.text.length < 2 ? '85%' : '50%'}}>{symbol.icon ? <img src={symbol.icon}/> : symbol.text}</span>}
         </div>
     );
