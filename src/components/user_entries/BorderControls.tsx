37b0817903d7c532efe59f3d5f61c241f171d371
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-23 18:10:57 -0400

    Add control for gradient

--- src/components/user_entries/BorderControls.tsx
+++ src/components/user_entries/BorderControls.tsx
@@ -18,6 +18,13 @@ export default function BorderControls()
                 <label htmlFor="border-colour">Colour:</label>
                 <input type="color" id="border-colour" value={borderData.colour} onChange={(e) => setBorderData({...borderData, colour: e.target.value})}/>
             </Control>
+            <Control>
+                <label htmlFor="border-gradient">Gradient:</label>
+                <Control>
+                    <input type="checkbox" id="border-gradient-enable" checked={borderData.useGradient} onChange={() => setBorderData({...borderData, useGradient: !borderData.useGradient})}/>
+                    <input type="color" id="editor-gradient" value={borderData.gradient} onChange={(e) => setBorderData({...borderData, gradient: e.target.value})}/>
+                </Control>
+            </Control>
         </div>
     );
 }
