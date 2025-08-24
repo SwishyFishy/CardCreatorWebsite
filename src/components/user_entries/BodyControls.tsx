64aac4cd3e104fdaf616c2ae1a79fd0dc3b1953a
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-23 23:09:05 -0400

    Add controls for body colour and opacity

--- src/components/user_entries/BodyControls.tsx
+++ src/components/user_entries/BodyControls.tsx
@@ -1,8 +1,35 @@
+import { useContext } from 'react';
+import { CONTEXT_cardData } from '../page/Layout';
+
+import type { BodyData } from '../card/unique_element/CardBody';
+
+import Control from '../common/Control';
+import ToggleableControl from '../common/ToggleableControl';
+import ColourInput from '../common/ColourInput';
+import RangeInput from '../common/RangeInput';
+
+import './styles/_controls.css';
+import './styles/body_controls.css';
+
 export default function BodyControls()
 {
+    const bodyData: BodyData = useContext(CONTEXT_cardData).cardData.body;
+    const setBodyData: Function = useContext(CONTEXT_cardData).functions.setBody;
+
     return(
-        <div id="component-bodycontrols">
-            <p>Hello Body</p>
+        <div id="component-bodycontrols" className="component-controls">
+            <Control>
+                <label htmlFor="body-colour">Colour:</label>
+                <ColourInput id="body-colour" value={bodyData.colour} SetColour={(e: any) => setBodyData({...bodyData, colour: e.target.value})}/>    
+            </Control>
+            <ToggleableControl toggle={bodyData.useGradient} SetToggle={() => setBodyData({...bodyData, useGradient: !bodyData.useGradient})}>
+                <label htmlFor="body-gradient">Gradient:</label>
+                <ColourInput id="body-gradient" value={bodyData.gradient} SetColour={(e: any) => setBodyData({...bodyData, gradient: e.target.value})}/>
+            </ToggleableControl>
+            <Control>
+                <label htmlFor="body-opacity">Opacity:</label>
+                <RangeInput id="body-opacity" min={0} max={1} step={0.01} value={bodyData.opacity} SetRange={(e: any) => setBodyData({...bodyData, opacity: e.target.value})}/>
+            </Control>
         </div>
     );
 }
