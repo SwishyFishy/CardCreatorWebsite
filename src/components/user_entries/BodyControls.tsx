1a5faa01dadabec847e771fa9857c09f720c3604
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-23 23:16:43 -0400

    Utilize 3 column centralized css in body and border controls panes, and add body content control

--- src/components/user_entries/BodyControls.tsx
+++ src/components/user_entries/BodyControls.tsx
@@ -18,18 +18,29 @@ export default function BodyControls()
 
     return(
         <div id="component-bodycontrols" className="component-controls">
-            <Control>
-                <label htmlFor="body-colour">Colour:</label>
-                <ColourInput id="body-colour" value={bodyData.colour} SetColour={(e: any) => setBodyData({...bodyData, colour: e.target.value})}/>    
-            </Control>
-            <ToggleableControl toggle={bodyData.useGradient} SetToggle={() => setBodyData({...bodyData, useGradient: !bodyData.useGradient})}>
-                <label htmlFor="body-gradient">Gradient:</label>
-                <ColourInput id="body-gradient" value={bodyData.gradient} SetColour={(e: any) => setBodyData({...bodyData, gradient: e.target.value})}/>
-            </ToggleableControl>
-            <Control>
-                <label htmlFor="body-opacity">Opacity:</label>
-                <RangeInput id="body-opacity" min={0} max={1} step={0.01} value={bodyData.opacity} SetRange={(e: any) => setBodyData({...bodyData, opacity: e.target.value})}/>
-            </Control>
+            <div className="column">
+                <Control>
+                    <label htmlFor="body-content">Content:</label>
+                    <textarea id="body-content" value={bodyData.content.join('\n')} onChange={(e) => (setBodyData({...bodyData, content: e.target.value.split('\n')}))}/>
+                </Control>
+            </div>
+            <div className="column">
+                <Control>
+                    <label htmlFor="body-colour">Colour:</label>
+                    <ColourInput id="body-colour" value={bodyData.colour} SetColour={(e: any) => setBodyData({...bodyData, colour: e.target.value})}/>    
+                </Control>
+                <ToggleableControl toggle={bodyData.useGradient} SetToggle={() => setBodyData({...bodyData, useGradient: !bodyData.useGradient})}>
+                    <label htmlFor="body-gradient">Gradient:</label>
+                    <ColourInput id="body-gradient" value={bodyData.gradient} SetColour={(e: any) => setBodyData({...bodyData, gradient: e.target.value})}/>
+                </ToggleableControl>
+                <Control>
+                    <label htmlFor="body-opacity">Opacity:</label>
+                    <RangeInput id="body-opacity" min={0} max={1} step={0.01} value={bodyData.opacity} SetRange={(e: any) => setBodyData({...bodyData, opacity: e.target.value})}/>
+                </Control>
+            </div>
+            <div className="column">
+
+            </div>
         </div>
     );
 }
--- src/components/user_entries/BorderControls.tsx
+++ src/components/user_entries/BorderControls.tsx
@@ -18,18 +18,26 @@ export default function BorderControls()
 
     return(
         <div id="component-bordercontrols" className="component-controls">
-            <Control>
-                <label htmlFor="border-colour">Colour:</label>
-                <ColourInput id="border-colour" value={borderData.colour} SetColour={(e: any) => setBorderData({...borderData, colour: e.target.value})}/>    
-            </Control>
-            <ToggleableControl toggle={borderData.useGradient} SetToggle={() => setBorderData({...borderData, useGradient: !borderData.useGradient})}>
-                <label htmlFor="border-gradient">Gradient:</label>
-                <ColourInput id="border-gradient" value={borderData.gradient} SetColour={(e: any) => setBorderData({...borderData, gradient: e.target.value})}/>
-            </ToggleableControl>
-            <Control>
-                <label htmlFor="border-thickness">Thickness:</label>
-                <RangeInput id="border-thickness" min={0} max={5} step={.1} value={borderData.thickness} SetRange={(e: any) => setBorderData({...borderData, thickness: e.target.value})}/>
-            </Control>
+            <div className="column">
+                <Control>
+                    <label htmlFor="border-colour">Colour:</label>
+                    <ColourInput id="border-colour" value={borderData.colour} SetColour={(e: any) => setBorderData({...borderData, colour: e.target.value})}/>    
+                </Control>
+                <ToggleableControl toggle={borderData.useGradient} SetToggle={() => setBorderData({...borderData, useGradient: !borderData.useGradient})}>
+                    <label htmlFor="border-gradient">Gradient:</label>
+                    <ColourInput id="border-gradient" value={borderData.gradient} SetColour={(e: any) => setBorderData({...borderData, gradient: e.target.value})}/>
+                </ToggleableControl>
+                <Control>
+                    <label htmlFor="border-thickness">Thickness:</label>
+                    <RangeInput id="border-thickness" min={0} max={5} step={.1} value={borderData.thickness} SetRange={(e: any) => setBorderData({...borderData, thickness: e.target.value})}/>
+                </Control>
+            </div>
+            <div className="column">
+
+            </div>
+            <div className="column">
+
+            </div>
         </div>
     );
 }
