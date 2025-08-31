6fa51e26c3feb212ead91db0129639dd53c30473
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-08-31 12:13:28 -0400

    Comment last commit

--- src/components/common/ColourInput.tsx
+++ src/components/common/ColourInput.tsx
@@ -10,6 +10,7 @@ export default function ColourInput({id, value, SetColour}: props_ColourInput)
 {
     return (
         <div className="component-colourinput">
+            {/* Regex matches any 6-digit hex code */}
             <input type="text" className={value.match(/^#(\d|[a-fA-F]){6}$/) || value == "Transparent" ? "" : "invalid"} value={value} onChange={(e) => SetColour(e)}/>
             <input type="color" className="transparent" id={id} value={value} onChange={(e) => SetColour(e)}/>
             <input type="button" value="Transparent" onClick={(e) => SetColour(e)}/>
