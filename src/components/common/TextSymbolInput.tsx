174614ebe4d072df694135ec763310d42b144bba
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-09-02 00:38:27 -0400

    Symbol library styling

--- src/components/common/TextSymbolInput.tsx
+++ src/components/common/TextSymbolInput.tsx
@@ -12,16 +12,11 @@ interface props_TextSymbolInput {
 
 export default function TextSymbolInput({id, value, SetText}: props_TextSymbolInput)
 {
-    const [showSymbols, setShowSymbols] = useState<boolean>(false);
-
     return(
         <div className="component-textsymbolinput">
             <textarea id={id} rows={5} value={value} onChange={(e) => SetText(e)}/>
             <div className="input-symbol">
-                <input type="button" className="add-symbol" value={`${showSymbols ? "Close" : "Add Symbol"}`} onClick={() => {setShowSymbols(!showSymbols)}}/>
-                <div className={`input-symbol-library ${showSymbols ? "visible" : "hidden"}`}>
-                    <SymbolLibrary/>
-                </div>
+                <SymbolLibrary/>
             </div>
         </div>
     );
