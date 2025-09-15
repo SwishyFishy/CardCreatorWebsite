6194db1e3deb6740f4c79a6f1897e71fd466e000
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-09-15 10:17:03 -0400

    Use autoisable on Control for clarity

--- src/components/common/ControlBackground.tsx
+++ src/components/common/ControlBackground.tsx
@@ -1,5 +1,3 @@
-import {v4 as uuid} from 'uuid';
-
 import Control from "./Control";
 import ColourInput from "./ColourInput";
 import RangeInput from './RangeInput';
@@ -15,53 +13,45 @@ interface props_ControlBackground {
 
 export default function ControlBackground({data, SetData}: props_ControlBackground)
 {
-    const baseId: string = uuid();
-
     return(
         <div className="component-controlbackground">   
             <Control>
-                <label htmlFor={`${baseId}-bg-colour`}>Colour:</label>
-                <ColourInput id={`${baseId}-bg-colour`} value={data.colour} SetColour={(e: any) => SetData({...data, colour: e.target.value})}/>    
+                <label>Colour:</label>
+                <ColourInput value={data.colour} SetColour={(e: any) => SetData({...data, colour: e.target.value})}/>    
             </Control>
             <Control>
-                <label htmlFor={`${baseId}-bg-add`}>Gradient:</label>
-                <input type="button" id={`${baseId}-bg-add`} value="Add Gradient" onClick={() => SetData({...data, gradient: data.gradient.toSpliced(data.gradient.length, 0, "#ffffff")})}/>
+                <label>Gradient:</label>
+                <input type="button" value="Add Gradient" onClick={() => SetData({...data, gradient: data.gradient.toSpliced(data.gradient.length, 0, "#ffffff")})}/>
             </Control>
             <ul>
                 {data.gradient.map((gradient, index) => (
                     <li key={index} className="gradient-selector">
                         <Control key={`${index}control`} deletable={{Delete: () => SetData({...data, gradient: data.gradient.toSpliced(index, 1)})}}>
-                            <label key={`${index}label`} htmlFor={`border-gradient-${index}`}>Gradient {index}:</label>
-                            <ColourInput key={`${index}colour`} id={`border-gradient-${index}`} value={gradient} SetColour={(e: any) => SetData({...data, gradient: data.gradient.toSpliced(index, 1, e.target.value)})}/>
+                            <label key={`${index}label`}>Gradient {index}:</label>
+                            <ColourInput key={`${index}colour`} value={gradient} SetColour={(e: any) => SetData({...data, gradient: data.gradient.toSpliced(index, 1, e.target.value)})}/>
                         </Control>
                     </li>
                 ))}
-                <li className={data.gradient.length > 0 ? "visible" : "hidden"}>
-                    <Control>
-                        <label htmlFor={`${baseId}-bg-gradient-type`}>Gradient Type:</label>
-                        <select id={`${baseId}-bg-gradient-type`} value={data.linear ? "linear" : "radial"} onChange={(e: any) => SetData({...data, linear: e.target.value == "linear" ? true : false})}>
-                            <option value="linear">Linear</option>
-                            <option value="radial">Radial</option>
-                        </select>
-                    </Control>
-                </li>
-                <li className={data.gradient.length > 0 && data.linear ? "visible" : "hidden"}>
-                    <Control>
-                        <label htmlFor={`${baseId}-bg-gradient-angle`}>Gradient Angle:</label>
-                        <input type="number" id={`${baseId}-bg-gradient-angle`} value={data.angle} onChange={(e: any) => SetData({...data, angle: e.target.value})}/>
-                    </Control>
-                </li>
-                <li className={data.gradient.length > 0 && !data.linear ? "visible" : "hidden"}>
-                    <Control>
-                        <label htmlFor={`${baseId}-bg-gradient-offsetX`}>Horizontal Gradient Offset (%):</label>
-                        <RangeInput id={`${baseId}-bg-gradient-offsetX`} min={0} max={100} step={1} value={data.offset?.x || 50} SetRange={(e: any) => SetData({...data, offset: {...data.offset, x: e.target.value}})}/>
-                    </Control>
-                    <Control>
-                        <label htmlFor={`${baseId}-bg-gradient-offsetY`}>Vertical Gradient Offset (%):</label>
-                        <RangeInput id={`${baseId}-bg-gradient-offsetY`} min={0} max={100} step={1} value={data.offset?.y || 50} SetRange={(e: any) => SetData({...data, offset: {...data.offset, y: e.target.value}})}/>
-                    </Control>
-                </li>
             </ul>
+            <Control autodisable={data.gradient.length == 0}>
+                <label>Gradient Type:</label>
+                <select value={data.linear ? "linear" : "radial"} onChange={(e: any) => SetData({...data, linear: e.target.value == "linear" ? true : false})}>
+                    <option value="linear">Linear</option>
+                    <option value="radial">Radial</option>
+                </select>
+            </Control>
+            <Control autodisable={data.gradient.length == 0 || !data.linear}>
+                <label>Gradient Angle:</label>
+                <input type="number" value={data.angle} onChange={(e: any) => SetData({...data, angle: e.target.value})}/>
+            </Control>
+            <Control autodisable={data.gradient.length == 0 || data.linear}>
+                <label>Horizontal Gradient Offset (%):</label>
+                <RangeInput min={0} max={100} step={1} value={data.offset?.x || 50} SetRange={(e: any) => SetData({...data, offset: {...data.offset, x: e.target.value}})}/>
+            </Control>
+            <Control autodisable={data.gradient.length == 0 || data.linear}>
+                <label>Vertical Gradient Offset (%):</label>
+                <RangeInput min={0} max={100} step={1} value={data.offset?.y || 50} SetRange={(e: any) => SetData({...data, offset: {...data.offset, y: e.target.value}})}/>
+            </Control>
         </div>
     );
 }
