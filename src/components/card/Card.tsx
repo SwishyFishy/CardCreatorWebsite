f24f64184405c200165ee2109dd9818dc4e4cd30
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-09-16 13:01:08 -0400

    Use useRef hook to set content height after applying view scale to ensure that footer isn't pushed out of frame

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useEffect, useRef, useContext } from "react";
 
 import CardBorder from "./unique_element/CardBorder";
 import CardArt from "./unique_element/CardArt";
@@ -14,6 +14,16 @@ import CardDetailBlock from "./duplicable_element/CardDetailBlock";
 export default function Card()
 {
     const cardData: CardData = useContext(CONTEXT_cardData).cardData;
+    const ref: React.RefObject<HTMLDivElement | null> = useRef(null);
+
+    // Set the content height dynamically based ont he final calculated height of the card
+    // Must be done this way because the height of the card is unknown until after the --card-view-scale is applied
+    useEffect(() => {
+        if (ref)
+        {
+            document.body.style.setProperty("--card-content-height", `calc(${ref.current!.clientHeight}px - var(--card-footer-dominance))`)
+        }
+    }, [ref.current?.clientHeight]);
 
     // Set CSS properties from card data
     document.body.style.setProperty("--card-height", `${cardData.card.height}mm`);
@@ -23,7 +33,7 @@ export default function Card()
     return(
         <div id="component-card">
             <CardBorder>
-                <div id="card">
+                <div id="card" ref={ref}>
                     <div id="card-main">
                         <CardArt/>
                         <CardBody/>
