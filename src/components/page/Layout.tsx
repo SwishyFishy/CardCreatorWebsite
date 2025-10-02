f4fe50e78ec1c68061c30bcb1e7e9777449637dc
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-10-02 13:29:23 -0400

    Default card

--- src/components/page/Layout.tsx
+++ src/components/page/Layout.tsx
@@ -1,15 +1,13 @@
 import { createContext, useState, useMemo } from "react";
 
 import BasicSymbols from "../symbol_library/BasicSymbols";
-import { type CardCore, type CardData, type CardBorder, type CardArt, type CardBody, type CardDetailGroup, type CardFooter, Character } from "../card/card_types";
+import { type CardCore, type CardData, type CardBorder, type CardArt, type CardBody, type CardDetailGroup, type CardFooter } from "../card/card_types";
 import type { SymbolData } from "../symbol_library/Symbol";
 
 import Header from "./Header";
 import Footer from "./Footer";
 import CardPane from "./CardPane";
 import DesignPane from "./DesignPane";
-
-import testArt from '../../assets/test/Necrolemur.jpg'; /*Remove from final prod*/
 
 import './styles/layout.css';
 
@@ -20,10 +18,8 @@ const init: CardData = {
     },
     border: {
         background: {
-            colour: "#000000", 
-            gradient: ["#ffffff", "#000000"],
-            linear: true, 
-            angle: 135
+            colour: "Transparent",
+            gradient: []
         },
         border: {
             colour: "#000000",
@@ -32,179 +28,50 @@ const init: CardData = {
             radius: 2.5
         }
     },
-    art: {
-        src: testArt,           /*Remove from default in final product*/
-        dominance: 60,
-        fullart: false,
-        gap: 2.5,
-        vShift: 0,
-        hShift: 0,
-        zoom: 150,
+    body: {
         background: {
-            colour: "transparent",
+            colour: "#999999",
             gradient: []
         },
         border: {
-            colour: "transparent",
-            thickness: 0,
-            inset: 0,
-            radius: 0
-        }
-    },
-    body: {
-        content: ["Rules", "text"],
-        background: {
-            colour: "#120fb6",
-            gradient: ["#f108eb"],
-            linear: false,
-        },
-        border: {
-            colour: "#ffffff",
+            colour: "Transparent",
             thickness: 0,
             inset: 0,
             radius: 0
         },
+        content: [],
         justify: "flex-start",
         align: "left",
-        opacity: .75
+        opacity: 1
+    },
+    art: {
+        background: {
+            colour: "Transparent",
+            gradient: []
+        },
+        border: {
+            colour: "Transparent",
+            thickness: 0,
+            inset: 0,
+            radius: 0
+        },
+        src: "",
+        dominance: 50,
+        fullart: false,
+        gap: 0,
+        vShift: 0,
+        hShift: 0,
+        zoom: 100
     },
     footer: {
         year: new Date().getFullYear(),
         set: "SET",
         collectorNum: 0,
         collectorMax: 0,
-        artist: "credit the artist"
+        artist: "credit your artist!"   
     },
-    details: [              /*Remove from default in final product*/
-        {
-            name: "titlebar",
-            elements: [{title: "Card Title", id: "title"}, {cost: [BasicSymbols[0]], direction: "row", id: "cost"}], 
-            align: "horizontal", 
-            justify: "first", 
-            position: "spread",
-            groupStyle: {
-                background: {
-                    colour: "transparent",
-                    gradient: []
-                },
-                border: 
-                {
-                    colour: "transparent",
-                    thickness: 0,
-                    radius: 0,
-                    inset: 0
-                },
-                textColour: "",
-                verticalText: "up"
-            },
-            elementStyles: [
-                {
-                    background: {
-                        colour: "#999999",
-                        gradient: [],
-                    },
-                    border: 
-                    {
-                        colour: "transparent",
-                        thickness: 0,
-                        radius: 0,
-                        inset: 0
-                    },
-                    textColour: "#2222ff",
-                    verticalText: "up"
-                },
-                {
-                    background: {
-                        colour: "transparent",
-                        gradient: []
-                    },
-                    border: 
-                    {
-                        colour: "transparent",
-                        thickness: 0,
-                        radius: 0,
-                        inset: 0
-                    },
-                    textColour: "#2222ff",
-                    verticalText: "up"
-                }
-            ]
-        },
-        {
-            name: "Typeline",
-            elements: [{types: ["Cardii", "Type"], id: "type"}], align: "horizontal", justify: "middle",  
-            position: "center",
-            groupStyle: {
-                background: {
-                    colour: "#666666",
-                    gradient: ["#ff6611"]
-                },
-                border: {
-                    colour: "#000000",
-                    thickness: .1,
-                    radius: 25,
-                    inset: .1
-                },
-                textColour: "#000000",
-                verticalText: "up"
-            },
-            elementStyles: [{
-                background: {
-                    colour: "transparent",
-                    gradient: []
-                },
-                    border: 
-                    {
-                        colour: "transparent",
-                        thickness: 0,
-                        radius: 0,
-                        inset: 0
-                    },
-                textColour: "",
-                verticalText: "up"
-            }]
-        },
-        {
-            name: "Stats", 
-            elements: [{stats: ["Card", "Stats"], separator: new Character('/'), id: "stats"}], 
-            align: "horizontal", 
-            justify: "last", 
-            position: "end",
-            groupStyle: {
-                background: {
-                    colour: "transparent",
-                    gradient: []
-                },
-                    border: 
-                    {
-                        colour: "transparent",
-                        thickness: 0,
-                        radius: 0,
-                        inset: 0
-                    },
-                textColour: "",
-                verticalText: "up"
-            },
-            elementStyles: [
-                {
-                    background: {
-                        colour: "#999999",
-                        gradient: ["#ff4444"]
-                    },
-                    border: 
-                    {
-                        colour: "#000000",
-                        thickness: 0.1,
-                        radius: 0,
-                        inset: 0.05
-                    },
-                    textColour: "#000000",
-                    verticalText: "up"
-                }
-            ]
-        }
-    ]
-};
+    details: []
+}
 
 export const CONTEXT_cardData: React.Context<{
     cardData: CardData, 
